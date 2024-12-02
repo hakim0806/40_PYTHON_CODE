95f5792dae66cc0e2f303d5e4b43593ed0518a8d
Abdul Hakim Akash <abdulhakimakash209@gmail.com>
2024-12-02 18:25:21 +0600

    2nd commit

--- pytoncode.py
+++ pytoncode.py
@@ -5,4 +5,14 @@ secnum = int(input("enter a number :"))
 add = firstnum + secnum
 #print(add)
 print(f"sum is = {add}")
+## 3.Write a program to calculate the sqrt of number.
+def sqr_root(n):
+    if n<0:
+        print("This number's root is not real")
+    else:
+        return n**0.5
  
+ 
+mynum = int(input("Enter a number = "))
+root_num = sqr_root(mynum)
+print(f"The root of{mynum} is = {root_num}")
