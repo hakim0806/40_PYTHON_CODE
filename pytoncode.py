37d0ae51b52a6925f752ef05d1c8eed8c239d493
Abdul Hakim Akash <abdulhakimakash209@gmail.com>
2024-12-02 18:20:36 +0600

    first commit

---
+++ pytoncode.py
@@ -0,0 +1,8 @@
+#2.Write a program to sum of two number.
+ 
+firstnum = int(input("enter a number :"))
+secnum = int(input("enter a number :"))
+add = firstnum + secnum
+#print(add)
+print(f"sum is = {add}")
+ 
